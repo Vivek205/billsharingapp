1cf1c9d07af08c124eefeba09dff27a2a1d37c90
Vivek205 <vnarayanan@travix.com>
2024-07-30 23:36:25 +0200

    ios App initial setup

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from "@capacitor/cli";
+
+const config: CapacitorConfig = {
+  appId: "dev.pages.billsharingweb",
+  appName: "billSharingApp",
+  webDir: "dist",
+};
+
+export default config;
